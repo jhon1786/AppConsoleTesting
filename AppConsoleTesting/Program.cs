2f2ded6c196fc51d6b4719ab95bb38f534999b40
jhon1786 <jhonavela086@gmail.com>
2026-02-08 17:16:29 -0500

    sera que ya

--- AppConsoleTesting/Program.cs
+++ AppConsoleTesting/Program.cs
@@ -6,4 +6,4 @@ Console.WriteLine("Esto es una prueba de git v1");
 
 Console.WriteLine("Esto es una prueba de git v2");
 
-Console.WriteLine("jhon eduardo velasquez villarraga ");
+Console.WriteLine("jhon eduardo velasquez villarraga");
